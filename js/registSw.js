5a4981981c6d0dc17eab3ae41b08e089711e5ed2
NaruakiFujii <naruaki_fujii@msr.co.jp>
2018-11-20 13:27:03 +0900

    他のサイトのに変えてみた

--- js/registSw.js
+++ js/registSw.js
@@ -1,16 +1,9 @@
-/**
- *
- */
-navigator.serviceWorker.register('/sw.js').then(function() {
-
-
-        console.log('サービスワーカーの登録成功');
-
-
-}).catch(function(err) {
-
-
-        console.log('サービスワーカーの登録ができませんでした：', err);
-
-
-});
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    // 登録成功
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }).catch(function(err) {
+    // 登録失敗 :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
