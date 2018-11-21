a6626536ca10615727ad1dfae48840af816bcc7a
NaruakiFujii <naruaki_fujii@msr.co.jp>
2018-11-21 14:51:28 +0900

    ページ遷移とかテスト

--- js/index.js
+++ js/index.js
@@ -1,8 +1,30 @@
-$(function(){
-	$('.arrive').click(function(){
-		alert("出社打刻しました");
+$(function() {
+	$('.loginbutton').click(function() {
+
+		var inputId = ($("input[name='id']").val() != "" || null);
+		var inputPass = ($("input[name='password']").val() != "" || null);
+
+		var data = {
+			inputId : ipnutId,
+			inputPass : inputPass
+		}
+		var loginStatus = checkLogin(data);
+
+		if (!loginStatus) {
+			$(this).parents('form').attr('action', location.pathname);
+		}
 	});
-	$('.leave').click(function(){
-		alert("退社打刻しました");
-	});
+
+	function checkLogin(id, pass) {
+		var fs = new ActiveXObject("Scripting.FileSystemObject");
+		var file = fs.OpenTextFile("C:\_dev\\testDB.txt");
+
+		var str = file.ReadLine();
+		var dbData = str.split(' ');
+
+		if (data.id === dbData[0] && data.pass === dbData[1]) {
+			return true;
+		}
+		return false;
+	}
 });
