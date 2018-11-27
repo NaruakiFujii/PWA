e19f2bb5554c8ab377e789b1d49b103e1b92ec6e
NaruakiFujii <naruaki_fujii@msr.co.jp>
2018-11-27 16:18:43 +0900

    asdfasfsfewfewfesafewsafesafef

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ $(function() {
 //		$.cookie("userId", inputId);
 //		$.cookie("userPass", inputPass);
 
-		var myHeaders = new Headers("none");
+		var myHeaders = new Headers("none", "none");
 		myHeaders.append("X-userData", data);
 
 	});
