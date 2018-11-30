07c44f3497a0284c627d80ea3da6a2696a115561
NaruakiFujii <naruaki_fujii@msr.co.jp>
2018-11-30 14:36:35 +0900

    位置情報

--- js/safetyReport.js
+++ js/safetyReport.js
@@ -2,7 +2,7 @@
 
 $(function(){
 
-	(function (){
+//	(function (){
 
 		// 初期処理でセッション情報を取得
 //		var id = window.sessionStorage.getItem(['userId']);
@@ -13,13 +13,13 @@ $(function(){
 //		}
 
 		//cookie
-		alert(JSON.parse($.cookie("userData")));
+//		alert(JSON.parse($.cookie("userData")));
 
 		//fetchAPI
 //		var myHeaders = new Headers();
 //		console.log(myHeaders.get("X-userData"));
 
-	})();
+//	})();
 
 	//値送る仕組みを後で入れる
 	$(document).on('click', '.report-button', function(){
