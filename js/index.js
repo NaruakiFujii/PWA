bde79c0c67ada94ffacbed85367cbe1233f833f4
NaruakiFujii <naruaki_fujii@msr.co.jp>
2018-11-30 17:17:18 +0900

    vueテスト

--- js/index.js
+++ js/index.js
@@ -14,66 +14,12 @@ $(function() {
 			location.href = location.href;
 		}
 
-		//session
-//		window.sessionStorage.setItem(['userId'], [inputId]);
-//		window.sessionStorage.setItem(['userPass'], [inputPass]);
-
 		//cookie
 		$.cookie("userData", JSON.stringify(data));
 
-		////fetchAPI試してみた
-		//Ajaxみたいな非同期通信だった…
-		//いずれ使える。使ってるとかっこいいかも…
-//		var myHeaders = new Headers();
-//		myHeaders.append("X-userData", data);
-//
-//		var url = "/PWA/views/timeStamp.html";
-//		fetch(url, {
-//			headers: {'X-userData': data}
-//		}).then(function(response){
-//			return response.json();
-//		});
 	});
 
 	function checkLogin(data) {
-		var url = "/PWA/testDB.txt";
-//		fetch(url)
-//			.then(function(response) {
-//				console.log(response.text());
-//				var dbData = response.text().PromiseVlue;
-//				if (data.id === dbData[0] && data.pass === dbData[1]) {
-//					return true;
-//				}
-//				return response.text();
-//			})
-//		fetch(url)
-//			.then((response) => response.body.getReader()) // ReadableStreamを取得する。
-//			.then((reader) => {
-			// ReadableStream.read()はPromiseを返す。
-			// Promiseは{ done, value }として解決される。
-			// データを読み込んだとき：doneはfalse, valueは値。
-			// データを読み込み終わったとき：doneはtrue, valueはundefined。
-//				function readChunk({done, value}) {
-//					if(done) {
-//						// 読み込みが終わっていれば最終的なテキストを表示する。
-//						console.log(veryLongText);
-//						return;
-//					}
-//
-//					var dbData = decoder.decode(value);
-//
-//					if (data.id === dbData[0] && data.pass === dbData[1]) {
-//						return true;
-//					}
-
-					// 次の値を読みにいく。
-//					reader.read().then(readChunk);
-//				}
-
-				// 最初の値を読み込む。
-//				reader.read().then(readChunk);
-//			})
-
 		//サーバーからデータ取ってきたい
 		var str = "0001 1";
 		var dbData = str.split(' ');
@@ -84,3 +30,20 @@ $(function() {
 		return false;
 	}
 });
+
+var here = "/PWA";
+var there = "/PWA/views/timeStamp";
+
+new Vue({
+	el: '.inputUserData',
+	routes: [
+		{ path: here, redirect: to => {
+			checkUser: function(){
+				if(this.id == 0001 && this.pass == 1){
+					return here;
+				}
+				return there;
+			}
+		}}
+	]
+})
